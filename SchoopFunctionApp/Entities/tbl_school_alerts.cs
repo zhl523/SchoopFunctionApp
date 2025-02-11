40b2379f83b31845766e5cb2b624afbe07867f3f
aliva523 <aliva@actionwebsites.com.au>
2025-02-11 18:03:15 +0800

    Init version

---
+++ SchoopFunctionApp/Entities/tbl_school_alerts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoopFunctionApp.Entities
+{
+    public partial class tbl_school_alerts
+    {
+        public int Alert_id { get; set; }
+        public int Schoop_id { get; set; }
+        public Nullable<System.DateTime> Alert_date_time { get; set; }
+        public string Activeyears { get; set; }
+        public bool Alert_urgent { get; set; }
+        public string Alert_text { get; set; }
+        public int Alert_sent_by { get; set; }
+        public Nullable<int> Language_id { get; set; }
+        public string ActiveGroups { get; set; }
+        public bool IsSent { get; set; }
+        public bool IsLoginAs { get; set; }
+        public bool IsTranslatedByGoogle { get; set; }
+        public Nullable<System.Guid> SurveyGuid { get; set; }
+        public bool IsScheduled { get; set; }
+        public Nullable<System.DateTime> ToSendTime { get; set; }
+        public Nullable<int> ParentAlertId { get; set; }
+        public Nullable<int> EventId { get; set; }
+        public Nullable<int> NewsId { get; set; }
+        public bool Deleted { get; set; }
+        public bool SendByDeviceIds { get; set; }
+        public string DeviceIds { get; set; }
+        public bool suggestedChannel { get; set; }
+        public Nullable<int> suggestedSchoopID { get; set; }
+        public bool IsPrivate { get; set; }
+        public bool IsContactFormAlert { get; set; }
+        public bool IsMigrateAlert { get; set; }
+        public bool IsInDocumentsDB { get; set; }
+        public bool IsSentForDeviceType5 { get; set; }
+    }
+}
---
+++ SchoopFunctionApp/Models/CollatedAlerts.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoopFunctionApp.Models
+{
+    public class CollatedAlerts
+    {
+        public int schoopID { get; set; }
+
+        public int alertID { get; set; }
+
+        public string alertDateTime { get; set; }
+
+        public string schoolName { get; set; }
+
+        public string activeYears { get; set; }
+
+        public string activeGroups { get; set; }
+
+        public string alertText { get; set; }
+
+        public bool alertUrgent { get; set; }
+
+        public int alertIcon { get; set; }
+
+        public int nID { get; set; } //news id
+        public int eID { get; set; } //event id
+        public string fID { get; set; } //survey url
+        public int sc { get; set; } //suggest schoopID
+
+        public CollatedAlerts(int schoopId, string school_name, int alert_ID, string alert_date_time, string active_years, string active_groups, bool alert_urgent, string alert_text, int alert_Icon, int nid, int eid, string fid, int suugestSchoopId)
+        {
+            schoopID = schoopId;
+            schoolName = school_name;
+            alertID = alert_ID;
+            alertDateTime = alert_date_time;
+            activeYears = string.IsNullOrEmpty(active_years) ? "" : active_years;
+            alertUrgent = alert_urgent;
+            alertText = string.IsNullOrEmpty(alert_text) ? "" : alert_text;
+            alertIcon = alert_Icon;
+            activeGroups = active_groups;
+            nID = nid;
+            eID = eid;
+            fID = fid;
+            sc = suugestSchoopId;
+        }
+    }
+}
